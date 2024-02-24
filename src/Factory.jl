afe9abf46f89d08c28ecf37cb7c2d5180ac9f0aa
ZachM63 <95306616+ZachM63@users.noreply.github.com>
2024-02-23 21:46:06 -0500

    Pset1

--- src/Factory.jl
+++ src/Factory.jl
@@ -1,8 +1,35 @@
 
 # ===== PRIVATE METHODS BELOW HERE =================================================================================== #
 # TODO: Put private helper methods here. Don't forget to use the _ naming convention, and to add basic documentation.
+
+function __characterbuild__(record)
+    characters = split(record, "")
+
+    return characters
+end
+
+function __lenbuild__(characters::Array{Char,1})
+    len = len(characters)
+
+    return len
+end
 # ===== PRIVATE METHODS ABOVE HERE =================================================================================== #
 
 # ===== PUBLIC METHODS BELOW HERE =================================================================================== #
 # TODO: Make a build method to construct MyPuzzleRecordModel types
+
+function build(type::Type{MyPuzzleRecordModel}, record::String)::MyPuzzleRecordModel
+    # Check to make sure everything is correct type
+
+    # initalizer
+    inst = MyPuzzleRecordModel()
+
+    # fill instance
+
+    inst.record = record
+    inst.characters = __characterbuild__(record)
+    inst.len = __lenbuild__(record)
+
+    return inst
+end
 # ===== PUBLIC METHODS ABOVE HERE =================================================================================== #
--- src/Types.jl
+++ src/Types.jl
@@ -4,5 +4,14 @@
 TODO: Add documentation
 """
 # TODO: Finish the MyPuzzleRecordModel type
+
 mutable struct MyPuzzleRecordModel
+
+    #data
+    record::String
+    characters::Array{Char, 1}
+    len::Int64
+
+    #constructor
+    MyPuzzleRecordModel() = new()
 end
